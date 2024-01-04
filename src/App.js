ab276ef1745ece441266ca1a02834743b8c19ec2
Khang <khangdev2813@gmail.com>
2024-01-04 23:12:00 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from "./logo.svg";
+import iu from "./iu.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Địt mẹ lồn Công</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={iu} className="App-logo" alt="logo" />
+        <p>Xoay nè</p>
       </header>
     </div>
   );
