5bf377c75847278030fba593d95358ac2d47fc4e
Khang <khangdev2813@gmail.com>
2024-01-04 23:14:13 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
           src="https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-1/325958149_642977074294574_2873835470172452296_n.jpg?stp=c0.0.320.320a_cp6_dst-jpg_p320x320&_nc_cat=100&ccb=1-7&_nc_sid=5740b7&_nc_eui2=AeERbWUcrTGq7IzUfz1B7AurnuZDB270MiOe5kMHbvQyI8sZEBYhv7Ltb5TIYu8alT1CJlbB-1aopp3Vz9VUa7Aj&_nc_ohc=dJN_ytTSiJEAX9AvSlr&_nc_ht=scontent.fsgn2-7.fna&oh=00_AfCXj0mwQdDZjp5QcwX25DVqZ-nyyNkNVrAaN8L4frHzIQ&oe=659C7110"
           className="App-logo"
           alt="logo"
+          style="border-radius: 50%;"
         />
         <p>Xoay nè</p>
       </header>
